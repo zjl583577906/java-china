a25941ca6e6910f0bdfb5ef9df9bdbe808de9d0f
biezhi <biezhi.me@gmail.com>
2016-02-21 21:04:52 +0800

    init project

---
+++ README.md
@@ -0,0 +1,4 @@
+# java-china
+
+Java中国论坛，使用Blade框架开发，演示地址 http://xxxx.com
+
---
+++ src/main/java/com/javachina/App.java
@@ -0,0 +1,13 @@
+package com.javachina;
+
+import com.blade.Blade;
+import com.blade.Bootstrap;
+
+public class App extends Bootstrap {
+
+	@Override
+	public void init(Blade blade) {
+		
+	}
+
+}
