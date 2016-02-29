036014fa6da4fda1a78664b4fb22a7024555ec98
biezhi <biezhi.me@gmail.com>
2016-02-29 17:22:16 +0800

    -

--- src/main/java/com/javachina/App.java
+++ src/main/java/com/javachina/App.java
@@ -2,15 +2,14 @@ package com.javachina;
 
 import com.blade.Blade;
 import com.blade.Bootstrap;
-import com.javachina.template.JetbrickTemplate;
+import com.blade.context.BladeWebContext;
+import com.bladejava.view.template.JetbrickTemplateEngine;
 
 public class App extends Bootstrap {
-
+	
 	@Override
 	public void init(Blade blade) {
-		blade.basePackage("com.javachina");
-		blade.routes("com.javachina.controller.admin");
-		blade.viewEngin(new JetbrickTemplate());
+		blade.viewEngin(new JetbrickTemplateEngine(BladeWebContext.servletContext()));
 	}
 
 }
