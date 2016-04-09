04a4556d9903a6cea2d9b5b19973cbae94b073cf
王爵 <biezhi.me@gmail.com>
2016-04-10 05:05:52 +0800

    not following self

--- src/main/webapp/assets/scripts/main.js
+++ src/main/webapp/assets/scripts/main.js
@@ -15,6 +15,22 @@ function go_signin(){
 		}
 	});
 }
+
+/* 
+弹出窗口居中 
+*/  
+function openWindow(url,name,iWidth,iHeight) {  
+    var url;                             //转向网页的地址;  
+    var name;                            //网页名称，可为空;  
+    var iWidth;                          //弹出窗口的宽度;  
+    var iHeight;                         //弹出窗口的高度;  
+    //获得窗口的垂直位置  
+    var iTop = (window.screen.availHeight-30-iHeight)/2;         
+    //获得窗口的水平位置  
+    var iLeft = (window.screen.availWidth-10-iWidth)/2;            
+    window.open(url,name,'height='+iHeight+',,innerHeight='+iHeight+',width='+iWidth+',innerWidth='+iWidth+',top='+iTop+',left='+iLeft+',status=no,toolbar=no,menubar=no,location=no,resizable=no,scrollbars=0,titlebar=no');  
+}  
+
 function alertError(msg){
 	swal({
 		title:"提示信息", 
